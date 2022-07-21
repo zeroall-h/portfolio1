7d6d11b34ec7a8daa11f016062fe6777d9c72eaa
zeroall <heodayeong@gmail.com>
2022-07-21 17:59:59 +0900

    반응형 css 수정

--- project1/js/javascript.js
+++ project1/js/javascript.js
@@ -1,10 +1,14 @@
-const header = document.querySelector("#header");
+const header = document.querySelector("header");
+const h1 = document.querySelector("h1");
 const headerHeight = header.getBoundingClientRect().height;
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > headerHeight) {
-    header.setAttribute("style", "background: black;");
-  } else {
+  if (window.scrollY < 10) {
     header.setAttribute("style", "background: transparent;");
+  } else if (window.scrollY < 700) {
+    header.setAttribute("style", "background: #fff;");
+  } else {
+    header.setAttribute("style", "background: #fff");
+    h1.setAttribute("style", "width: 10rem");
   }
 });
